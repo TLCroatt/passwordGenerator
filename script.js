13b4036947cc5c0d2793d16880273abb4a5585bc
TLCroatt <trishacroatt@gmail.com>
2020-07-10 16:39:15 -0600

    created character vars

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var numeric = "1234567890"
+var specialChar = "!@#$%^&*?"
+var selectedChar = ""
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-
-  function generatePassword() {
-  
-    while(confirmLength <=8 || confirmLength >=128) {
-      var confirmLength = prompt("Password length may be between 8 - 128 characters. How long would you like your password to be?")
-    } 
-  }
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//create 4 character type strings (vars) - special chars, numeric, lower case, upper case
+//create a function to get all user input - length, inclusion of characters: upper, lower, numeric, special; validate - if user gives garbage input, stop function
+//combine all strings for selected characters into one - store in a var
+//pick from the super string as many times randomly for as long as the password is (n times) - store in a var
+//write the string to html (line 16 - already done)
